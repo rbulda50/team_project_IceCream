4714c272a772227ec510f5a0385789b3266ea39f
rbulda50 <117113002+rbulda50@users.noreply.github.com>
2022-12-17 17:57:22 +0200

    feature/modals-connection
    created modal-script: modal-buy, modal-ingredients, modal-made; these modals were connected

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import './js/slider.js';
 import './js/modal-script';
 import './js/mobile-menu';
-import './js/gallery'
+import './js/gallery';
+import './js/modal-buy';
+import './js/modal-ingredients';
+import './js/modal-made';
