7e163aabfae924aa86370b2dc9bd8acb06c4722f
rbulda50 <117113002+rbulda50@users.noreply.github.com>
2022-12-19 02:05:04 +0200

    bugfix/all-sections
    added script for button "submit"; no-scroll for backdrop

--- src/js/modal-made.js
+++ src/js/modal-made.js
@@ -3,12 +3,14 @@
     openModalBtn: document.querySelector("[made-modal-open]"),
     closeModalBtn: document.querySelector("[made-modal-close]"),
     modal: document.querySelector("[made-modal]"),
+
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
+    document.body.classList.toggle("modal-open");
     refs.modal.classList.toggle("is-hidden");
   }
 })();
