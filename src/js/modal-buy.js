5661a603b26ae007658210145fa3f230da2551e2
rbulda50 <117113002+rbulda50@users.noreply.github.com>
2022-12-20 19:05:31 +0200

    feature/pattern
    added pattern in inputs;

--- src/js/modal-buy.js
+++ src/js/modal-buy.js
@@ -3,14 +3,12 @@
     openModalBtn: document.querySelector("[data-buy-modal-open]"),
     openModalBuySecondBtn: document.querySelector("[data-buy-second-modal-open]"),
     closeModalBtn: document.querySelector("[data-buy-modal-close]"),
-    sendModalBtn: document.querySelector("[data-buy-modal-send]"),
     modal: document.querySelector("[data-buy-modal]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.openModalBuySecondBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
-  refs.sendModalBtn.addEventListener("click", toggleModal,);
 
   function toggleModal() {
     document.body.classList.toggle("modal-open");
--- src/js/modal-franchise.js
+++ src/js/modal-franchise.js
@@ -2,13 +2,11 @@
     const refs = {
       openModalBtn: document.querySelector("[data-franchise-modal-open]"),
       closeModalBtn: document.querySelector("[data-franchise-modal-close]"),
-      sendModalBtn: document.querySelector("[data-franchise-modal-send]"),
       modal: document.querySelector("[data-franchise-modal]"),
     };
   
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
-    refs.sendModalBtn.addEventListener("click", toggleModal,);
   
     function toggleModal() {
       document.body.classList.toggle("modal-open");
